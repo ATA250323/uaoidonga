e3e41a07cd2436c3532665a04b876d5b8c0df52f
unknown <abtoure99@gmail.com>
2026-01-21 13:07:00 +0100

    commentaire 21/01/26

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.appsite')
+@extends('layouts.appsiteconnexion')
 
 @section('content')
 <div class="container-fluid py-5">
---
+++ resources/views/layouts/partifixe/footerconnexion.blade.php
@@ -0,0 +1,20 @@
+    <div class="container-fluid bg-dark text-white-50 footer pt-5">
+        <div class="container wow fadeIn" data-wow-delay="0.1s">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a class="border-bottom" href="#!">{{ __('traduction.uaoidonga')}}</a>, {{ __('traduction.rights')}}
+                    </div>
+                    {{-- <div class="col-md-6 text-center text-md-end">
+                        <div class="footer-menu">
+                            <a href="#!">Home</a>
+                            <a href="#!">Cookies</a>
+                            <a href="#!">Help</a>
+                            <a href="#!">FAQs</a>
+                        </div>
+                    </div> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+ <!-- Footer End -->
